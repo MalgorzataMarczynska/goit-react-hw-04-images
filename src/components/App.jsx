8d357abd348ec047d4f6b47313893399c7edef87
MalgorzataMarczynska <malgorzatamarczynska83@gmail.com>
2023-02-26 20:20:07 +0100

    image finder hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { ImageGallery } from './ImageGallery/ImageGallery.js';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem.js';
 import { Searchbar } from './searchbar/Searchbar.js';
@@ -7,134 +7,107 @@ import { Modal } from './Modal/Modal.js';
 import { Button } from './Button/Button.js';
 import { FallingLines } from 'react-loader-spinner';
 
-export class App extends React.Component {
-  state = {
-    images: [],
-    searchQuery: '',
-    IsLoading: false,
-    error: null,
-    modalIsOpen: false,
-    currentImageClicked: {},
-    page: 1,
-  };
-  input = React.createRef();
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [currentImageClicked, setCurrentImageClicked] = useState({});
+  const [searchPage, setSearchPage] = useState(1);
+  const input = useRef('');
 
-  handleRequest = async (searchQuery = 'sun', page) => {
-    this.setState({ isLoading: true });
-    try {
-      const data = await fetchImagesWithQuery(
-        searchQuery,
-        (page = this.state.page)
-      );
-      const totalPages = Math.ceil(data.totalHits / 12);
-      const images = data.hits;
-      if (this.state.page > totalPages) {
-        return alert('You have reached end of results or we did not find any');
-      }
-      this.setState(prevState => ({
-        images: [...prevState.images, ...images],
-      }));
-    } catch (error) {
-      this.setState({ error });
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  };
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const searchQuery = this.input.current.value;
-    if (this.state.searchQuery === searchQuery) return;
-    this.setState({ searchQuery, page: 1, images: [] });
+    const searchInput = input.current.value;
+    if (query === searchInput) return;
+    setQuery(searchInput);
+    setSearchPage(1);
+    setImages([]);
   };
-  handlePage = () => {
-    const page = this.state.page + 1;
-    this.setState({ page });
+  const handlePage = () => {
+    const newPage = searchPage + 1;
+    setSearchPage(newPage);
   };
 
-  async componentDidMount() {
-    this.handleRequest();
-  }
-  componentDidUpdate(_prevProps, prevState) {
-    if (
-      prevState.searchQuery !== this.state.searchQuery ||
-      this.state.page !== prevState.page
-    ) {
-      this.handleRequest(this.state.searchQuery, this.state.page);
-    }
-  }
+  useEffect(() => {
+    const handleRequest = async (searchQuery = 'sun', page) => {
+      setIsLoading(true);
+      try {
+        const data = await fetchImagesWithQuery(query, (page = searchPage));
+        const totalPages = Math.ceil(data.totalHits / 12);
+        const newImages = data.hits;
+        if (searchPage > totalPages) {
+          return alert(
+            'You have reached end of results or we did not find any'
+          );
+        }
+        setImages([...images, ...newImages]);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    handleRequest();
+  }, [query, searchPage]);
 
-  checkModalKey = e => {
+  const checkModalKey = e => {
     if (e.code === 'Escape') {
-      this.closeModal();
+      closeModal();
     }
   };
-  checkModalOverlay = evt => {
+  const checkModalOverlay = evt => {
     if (evt.target.hasAttribute('data-action')) {
-      this.closeModal();
+      closeModal();
     }
   };
 
-  closeModal = () => {
-    document.removeEventListener('keydown', this.checkModalKey);
-    document.removeEventListener('click', this.checkModalOverlay);
-    this.setState({
-      modalIsOpen: false,
-    });
+  const closeModal = () => {
+    document.removeEventListener('keydown', checkModalKey);
+    document.removeEventListener('click', checkModalOverlay);
+    setModalIsOpen(false);
   };
-  toggleModal = e => {
-    if (!this.state.modalIsOpen) {
-      document.addEventListener('keydown', this.checkModalKey);
-      document.addEventListener('click', this.checkModalOverlay);
+  const toggleModal = e => {
+    if (!modalIsOpen) {
+      document.addEventListener('keydown', checkModalKey);
+      document.addEventListener('click', checkModalOverlay);
     } else {
-      document.removeEventListener('keydown', this.checkModalKey);
-      document.removeEventListener('click', this.checkModalOverlay);
+      document.removeEventListener('keydown', checkModalKey);
+      document.removeEventListener('click', checkModalOverlay);
     }
     e.preventDefault();
     const clickedUrl = e.currentTarget.href;
     const clickedImageAlt = e.target.alt;
     const clickedObject = { clickedUrl, clickedImageAlt };
-    this.setState({
-      modalIsOpen: !this.state.modalIsOpen,
-      currentImageClicked: clickedObject,
-    });
+    setModalIsOpen(!modalIsOpen);
+    setCurrentImageClicked(clickedObject);
   };
 
-  render() {
-    const { images, isLoading, error, modalIsOpen, currentImageClicked } =
-      this.state;
-
-    return (
-      <div
-        ref={node => {
-          this.node = node;
-        }}
-      >
-        <Searchbar onSubmit={this.handleSubmit} input={this.input} />
-        {error && <p>Sorry, something went really wrong: {error.message}</p>}
-        {isLoading && (
-          <FallingLines
-            color="#3f51b5"
-            width="100"
-            visible={true}
-            ariaLabel="falling-lines-loading"
-          />
-        )}
-        {images.length > 0 && !modalIsOpen ? (
-          <ImageGallery>
-            <ImageGalleryItem
-              images={this.state.images}
-              toggleModal={this.toggleModal}
-            />
-            <Button nextPageLoader={this.handlePage} />
-          </ImageGallery>
-        ) : (
-          <Modal
-            images={this.state.images}
-            onClose={this.toggleModal}
-            currentImageClicked={currentImageClicked}
-          ></Modal>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Searchbar onSubmit={handleSubmit} input={input} />
+      {error && <p>Sorry, something went really wrong: {error.message}</p>}
+      {isLoading && (
+        <FallingLines
+          color="#3f51b5"
+          width="100"
+          visible={true}
+          ariaLabel="falling-lines-loading"
+        />
+      )}
+      {images.length > 0 && !modalIsOpen ? (
+        <ImageGallery>
+          <ImageGalleryItem images={images} toggleModal={toggleModal} />
+          <Button nextPageLoader={handlePage} />
+        </ImageGallery>
+      ) : (
+        <Modal
+          images={images}
+          onClose={toggleModal}
+          currentImageClicked={currentImageClicked}
+        ></Modal>
+      )}
+    </div>
+  );
+};
